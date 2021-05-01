9556eacfc16f76fabc53d720c6d10468e97bc72f
mmhousto <mmhouston6@live.com>
2021-04-30 21:22:25 -0500

    Added torch lighting

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     Rigidbody arrowRB;
-    private float lifeTimer = 2f;
+    private float lifeTimer = 6f;
     private float timer;
     private bool hit;
+    Animator anim;
+    private float rotateZ;
 
     // Start is called before the first frame update
     void Start()
@@ -15,12 +17,16 @@ public class Arrow : MonoBehaviour
         timer = lifeTimer;
         arrowRB = GetComponent<Rigidbody>();
         hit = false;
+        //anim = GetComponent<Animator>();
+        //anim.SetBool("inAir", true);
+        rotateZ = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         //transform.right = arrowRB.velocity;
+        rotateZ += 2.0f;
 
         //life timer
         timer -= Time.deltaTime;
@@ -28,17 +34,21 @@ public class Arrow : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        transform.Rotate(new Vector3(0f, 0f, rotateZ));
 
+    }
+
+    void LateUpdate()
+    {
         if (arrowRB.velocity != Vector3.zero && hit == false)
         {
             transform.rotation = Quaternion.LookRotation(arrowRB.velocity);
         }
-
     }
-
 
     private void OnCollisionEnter(Collision collision)
     {
+        //anim.SetBool("inAir", false);
         arrowRB.isKinematic = true;
         hit = true;
         if(collision.collider.tag == "target")
--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -22,7 +22,7 @@ public class MouseLook : MonoBehaviour
         float pitch = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= pitch;
-        xRotation = Mathf.Clamp(xRotation, -75f, 55f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 55f);
         
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * yaw);
